a7df8bfed6c13fdfdb079048adb78e51cef5a8b4
Derek Rushforth <derekrushforth@gmail.com>
2019-01-02 11:48:18 -0700

    Add boilerplate for tests

---
+++ test/postmark-templates.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const extend = require('extend');
+
+describe('postmark-templates', () => {
+
+  describe('test', () => {
+
+    it('it should return highlighted JSON', () => {
+      assert.equal(true, true);
+    });
+  });
+
+});

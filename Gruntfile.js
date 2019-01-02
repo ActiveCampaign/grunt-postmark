41b39ae92c413f4316752f7f8062616f20660072
Derek Rushforth <derekrushforth@gmail.com>
2019-01-02 16:37:47 -0700

    [WIP] Template pushing
    - Simplify workflow and configuration
    - Review changed files before pushing templates

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,49 +30,13 @@ module.exports = function (grunt) {
         src: ["test/emails/*.html"]
       }
     },
-
-    "postmark-templates-upload": {
+    "postmark-templates-push": {
       options: {
-        ephemeralUploadResultsProperty: "<%= config.templates && config.templates.ephemeralUploadResultsProperty %>",
-        serverToken: "<%= secret.postmark.server_token %>"
-      },
-      test_email: {
-        name: "testing-postmark-templates-js1-" + new Date().valueOf(),
-        subject: "Testing grunt-postmark-templates",
-        htmlSrc: "test/email.html",
-        textSrc: "test/email.txt"
-      },
-      test_email_again: {
-        name: "testing-postmark-templates-js2-" + new Date().valueOf(),
-        subject: "Testing grunt-postmark-templates (again)",
-        htmlSrc: "test/email.html",
-        textSrc: "test/email.txt"
-      },
-      test_email_inline_body: {
-        name: "testing-postmark-templates-js3-" + new Date().valueOf(),
-        subject: "Testing grunt-postmark-templates (inline body)",
-        htmlBody: "<html><body><h1>Another email test</h1></body></html>",
-        textBody: "Hello from grunt-postmark-templates\n"
-      }
-    },
-
-    "postmark-templates-output": {
-      options: {
-          cleanOutput: "<%= config.templates && config.templates.cleanOutput %>",
-          outputFile: "<%= config.templates && config.templates.outputFile || config.templates && config.templates.file %>",
-          ephemeralUploadResultsProperty: "<%= config.templates && config.templates.ephemeralUploadResultsProperty %>"
-      }
-    },
-
-    "postmark-templates-parse": {
-      options: {
-        inputFile: "<%= config.templates && config.templates.inputFile || config.templates && config.templates.file %>"
+        templates: "<%= config.templates %>"
       }
     }
   });
 
   grunt.loadTasks("tasks");
-
-  grunt.registerTask("default", ["postmark", "postmark-templates"]);
-
+  grunt.registerTask("default", ["postmark"]);
 };
--- tasks/grunt-postmark-templates.js
+++ tasks/grunt-postmark-templates.js
@@ -5,150 +5,216 @@
  * https://github.com/wildbit/grunt-postmark.git
  */
 
-module.exports = function (grunt) {
-  var DEFAULT_EPHEMERAL_UPLOAD_RESULTS_PROPERTY = 'postmark-templates-upload-results';
-  var DEFAULT_OUTPUT_FILE_NAME = 'templates.json';
-  var DEFAULT_CLEAN_OUTPUT = false;
+module.exports = (grunt) => {
+  const _ = require('lodash');
+  const inquirer = require('inquirer');
+  const table = require('table');
 
-  grunt.registerTask('postmark-templates-parse', 'Parse Postmark templates for update', function () {
-    var options = this.options({
-      inputFile: DEFAULT_OUTPUT_FILE_NAME
-    });
-    var templateObjects = grunt.file.readJSON(options.inputFile || DEFAULT_OUTPUT_FILE_NAME);
-
-    grunt.config.set('postmark-templates-upload', Object.assign(
-      templateObjects,
-      {
-        options: grunt.config.get('postmark-templates-upload.options')
-      }
-    ));
-  });
-
-  grunt.registerMultiTask('postmark-templates-upload', 'Create or update Postmark templates', function () {
+  grunt.registerTask('postmark-templates-push', 'Push templates to Postmark', function() {
+    this.results = {
+      completed: 0,
+      success: 0,
+      failed: 0
+    };
+    this.localTemplates = [];
     var done = this.async();
-    var options = this.options({
-      ephemeralUploadResultsProperty: DEFAULT_EPHEMERAL_UPLOAD_RESULTS_PROPERTY
-    });
-    var ephemeralUploadResultsProperty = options.ephemeralUploadResultsProperty || DEFAULT_EPHEMERAL_UPLOAD_RESULTS_PROPERTY;
-    var template = this.data;
-
-    var serverToken = options.serverToken || grunt.config('secret.postmark.server_token');
+    var options = this.options();
+    var templates = options.templates;
+    var serverToken = grunt.config('secrets.postmarkServerToken');
 
     if (!serverToken) {
       grunt.fail.warn('Missing Postmark server token \n');
     }
 
-    if (!template.name) {
-      grunt.fail.warn('Missing required template property "name" \n');
+    if (!Array.isArray(templates)) {
+      grunt.fail.warn('Templates config must be an array containing objects \n')
     }
 
-    if (!template.subject) {
-      grunt.fail.warn('Missing required template property "subject" \n');
-    }
-
-    if (!template.htmlBody && !template.htmlSrc) {
-      grunt.log.error('Missing template property "htmlBody" or "htmlSrc"\n');
-    }
-
-    if (!template.textBody && !template.textSrc) {
-      grunt.log.error('Missing template property "textBody" or "textSrc"\n');
-    }
-
+    // Set up Postmark client
     var postmark = require('postmark');
     var client = new postmark.Client(serverToken);
 
-    // read the referenced files, but hold on to the original filenames
-    var expanded = {
-      Name: template.name,
-      Subject: template.subject,
-      HtmlBody: template.htmlBody || grunt.file.read(template.htmlSrc),
-      TextBody: template.textBody || grunt.file.read(template.textSrc),
-      TemplateId: template.templateId
-    };
+    // Get templates from server
+    client.getTemplates().then(result => {
+      let review = [];
 
-    if (expanded.TemplateId) {
-      client.editTemplate(expanded.TemplateId, expanded, function (err, response) {
-        if (err && err.code === 1101) {
-          grunt.log.warn('Template ' + expanded.TemplateId + ' not found, so attempting create');
-          delete template.templateId;
-          delete expanded.TemplateId;
-          client.createTemplate(expanded.TemplateId, function (err, response) {
-            grunt.log.writeln('Template ' + expanded.Name + ' created: ' + JSON.stringify(response.TemplateId));
-            handleResponse(err, done, response, template, ephemeralUploadResultsProperty);
+      // Validate template objects and parse local templates
+      templates.forEach((template) => {
+        validateTemplate(template);
+
+        // Determine template's Alias or ID
+        const identifier = getIdentifier(template);
+        const existingTemplate = _.find(result.Templates, identifier);
+
+        // Throw error if template with ID is not on server
+        if (identifier.TemplateId && _.isEmpty(existingTemplate)) {
+          grunt.fail.fatal(`${template.name}: This template has an ID that is not on your Postmark server. You’ll need to use a correct ID or enter an alias to push this template.`)
+        }
+
+        // Gather email content
+        const htmlBody = template.htmlBody ? grunt.file.read(template.htmlBody) : '';
+        const textBody = template.textBody ? grunt.file.read(template.textBody) : '';
+
+        // Temporarily store this template's info
+        this.localTemplates.push({
+          ...identifier,
+          New: _.isEmpty(existingTemplate),
+          Name: template.name,
+          Subject: template.subject,
+          HtmlBody: htmlBody,
+          TextBody: textBody
+        });
+
+        // Add template to review list
+        const reviewChangeType = existingTemplate ? 'Modified'.yellow : 'Added'.green;
+        review.push([reviewChangeType, template.name, template.alias || `ID: ${template.id}`]);
+      });
+
+      // Show files that are changing
+      printReview(review);
+
+      // Ask user to confirm push
+      confirmPush(done);
+    }).catch(error => {
+      grunt.fail.fatal(error);
+      done(false);
+    });
+
+    /**
+     * [getIdentifier description]
+     * @param  {[type]} template [description]
+     * @return {[type]}          [description]
+     */
+    var getIdentifier = (template) => {
+      if (template.alias) return { Alias: template.alias };
+      if (template.id) return { TemplateId: template.id };
+    }
+
+    /**
+     * Ask user to confirm push
+     * @param  {Function} done
+     */
+    var confirmPush = (done) => {
+      inquirer.prompt([{
+        type: 'confirm',
+        name: 'push',
+        default: false,
+        message: 'Are you sure you want to push these templates to Postmark?'
+      }]).then(answers => {
+        if (answers.push) {
+          grunt.log.writeln('Right on. Pushing your templates to Postmark...');
+
+          this.localTemplates.forEach((template, index) => {
+            pushTemplate(template, done)
           });
         } else {
-          grunt.log.writeln('Template ' + expanded.Name + ' updated: ' + JSON.stringify(response.TemplateId));
-          handleResponse(err, done, response, template, ephemeralUploadResultsProperty);
+          grunt.log.writeln('Canceling template push.');
+          done();
         }
       });
-    } else {
-      client.createTemplate(expanded, function (err, response) {
-        grunt.log.writeln('Template ' + expanded.Name + ' created: ' + JSON.stringify(response.TemplateId));
-        handleResponse(err, done, response, template, ephemeralUploadResultsProperty);
-      });
     }
-  });
 
-  function handleResponse(err, done, response, template, ephemeralUploadResultsProperty) {
-    if (err){
-      errorMessage(err);
-      done();
-    } else {
-      template.templateId = response.TemplateId;
-      // compile the templates for use by the `postmark-templates-output` task
-      var updatedTemplates = grunt.config.get(ephemeralUploadResultsProperty) || {};
-      updatedTemplates[template.name] = template;
-      grunt.config.set(ephemeralUploadResultsProperty, updatedTemplates);
-
-      done();
-    }
-  }
-
-  function errorMessage(err) {
-    if (err.message) {
-      grunt.log.warn('Error: ' + err.message);
-    } else {
-      grunt.log.warn('Error: ' + JSON.stringify(err));
-    }
-  }
-
-  // invoke this task after postmark-templates to get an output file containing the resulting template IDs
-  // this is in the same format as the postmark-templates config.
-
-  grunt.registerTask('postmark-templates-output', 'Write out the resulting template IDs', function () {
-    var options = this.options({
-      cleanOutput: DEFAULT_CLEAN_OUTPUT,
-      outputFile: DEFAULT_OUTPUT_FILE_NAME,
-      ephemeralUploadResultsProperty: DEFAULT_EPHEMERAL_UPLOAD_RESULTS_PROPERTY
-    });
-    var ephemeralUploadResultsProperty = options.ephemeralUploadResultsProperty || DEFAULT_EPHEMERAL_UPLOAD_RESULTS_PROPERTY;
-    var outputFile = options.outputFile || DEFAULT_OUTPUT_FILE_NAME;
-    var cleanOutput = options.cleanOutput || DEFAULT_CLEAN_OUTPUT;
-    var updatedTemplates = grunt.config(ephemeralUploadResultsProperty);
-    var oldTemplates = grunt.file.exists(outputFile)
-      ? grunt.file.read(outputFile)
-      : {};
-
-    Object.keys(updatedTemplates).forEach(function (updatedTemplateKey) {
-      updatedTemplates[updatedTemplateKey] = Object.assign(
-        oldTemplates[updatedTemplateKey] || {},
-        updatedTemplates[updatedTemplateKey]
-      );
-
-      if (cleanOutput) {
-        delete updatedTemplates[updatedTemplateKey].htmlBody;
-        delete updatedTemplates[updatedTemplateKey].textBody;
-        delete updatedTemplates[updatedTemplateKey].htmlSrc;
-        delete updatedTemplates[updatedTemplateKey].textSrc;
+    /**
+     * Add a new template or modify an existing one
+     * @param  {Object}   template
+     * @param  {Function} done
+     */
+    var pushTemplate = (template, done) => {
+      if (template.New) {
+        // Add new template
+        client.createTemplate(template).then(response => {
+          pushComplete({
+            success: true,
+            response,
+            template,
+            done
+          });
+        }).catch(response => {
+          pushComplete({
+            success: false,
+            response,
+            template,
+            done
+          });
+        });
+      } else {
+        // Modify existing template
+        const identifier = template.Alias || template.TemplateId;
+        client.editTemplate(identifier, template).then(response => {
+          pushComplete({
+            success: true,
+            response,
+            template,
+            done
+          });
+        }).catch(response => {
+          pushComplete({
+            success: true,
+            response,
+            template,
+            done
+          });
+        });
       }
-    });
+    }
 
-    grunt.file.write(outputFile, JSON.stringify(updatedTemplates, null, 2));
-    grunt.log.writeln("Updated template information written to " + outputFile);
+    /**
+     * Execute each time a template is pushed
+     * @param  {Object} result
+     */
+    var pushComplete = (result) => {
+      const { success, response, template, done } = result;
+
+      // Update counters
+      this.results.completed++;
+      this.results[success ? 'success' : 'failed']++;
+
+      // Log errors to the console
+      if (!success) {
+        grunt.log.writeln(`${template.Name}: ${response.toString()}`.red);
+      }
+
+      // Last template pushed
+      if (this.results.completed === this.localTemplates.length) {
+        // Log results
+        grunt.log.writeln(`${this.results.success} templates were pushed successfully.`.green);
+        grunt.log.writeln(`${this.results.failed} ${grunt.util.pluralize(this.results.failed, 'template/templates')} failed. Please see the output above for more details.`.red);
+
+        done();
+      }
+    }
+
+    /**
+     * Prints changed files to the console
+     * @param  {Array} review List of changed files
+     */
+    var printReview = (review) => {
+      let output = [['Type'.gray, 'Template'.gray, 'Alias'.gray], ...review]
+      grunt.log.writeln(table.table(output, { border: table.getBorderCharacters('norc')}));
+    }
+
+    /**
+     * Validate the template configuration
+     * @param  {Object} template
+     */
+    var validateTemplate = (template) => {
+      if (!template.alias && !template.id) {
+        grunt.fail.fatal('One of your templates is missing an alias and id. Either of these are required in order to push your templates. Aliases are recommended so it’s easier to track the same template across multiple servers. Check out https://postmarkapp.com/support/article/1117-how-do-i-use-a-template-alias for more details.');
+      }
+
+      const identifier = template.alias || template.id;
+
+      if (!template.name) {
+        grunt.fail.fatal(`${identifier}: Please enter a name field.`);
+      }
+
+      if (!template.subject) {
+        grunt.fail.fatal(`${template.name}: Please enter a subject field.`);
+      }
+
+      if (!template.htmlBody && !template.textBody) {
+        grunt.fail.fatal(`${template.name}: Please enter either an htmlBody, textBody, or both.`);
+      }
+    }
   });
-
-  // you can also get a JSON report of uploaded templates
-  grunt.registerTask('postmark-templates-from-targets', ['postmark-templates-upload', 'postmark-templates-output']);
-  grunt.registerTask('postmark-templates-from-file', ['postmark-templates-parse', 'postmark-templates-from-targets']);
-  grunt.registerTask('postmark-templates', ['postmark-templates-from-targets']);
 };
